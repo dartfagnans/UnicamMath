46a9868e480e735fe61839747176570d50c8204b
giusfag <giuseppe.fagnani@studenti.unicam.it>
2017-04-05 09:25:23 +0200

    Migliorato il codice: uso di un metodo per chiedere il cateto all'utente

--- Program.cs
+++ Program.cs
@@ -4,6 +4,21 @@ namespace UnicamMath
 {
     class Program
     {
+        public static double ChiediCatetoAllUtente (String messaggio) {
+                    //Ciclo infinito per chiedere un valore corretto di cateto1
+                while (true)
+                {
+                    Console.WriteLine(messaggio);
+                    String catetoString = Console.ReadLine();
+                    if (double.TryParse(catetoString, out double cateto))
+                        return cateto;
+                    else
+                    {
+                        Console.WriteLine($"{catetoString} non è un valore corretto");
+                        continue;
+                    }
+                }
+                }
         static void Main(string[] args)
         {
             while (true)
@@ -23,32 +38,9 @@ namespace UnicamMath
                 string cateto1Stringa = "";
                 string cateto2Stringa = "";
 
-                //Ciclo infinito per chiedere un valore corretto di cateto1
-                while (true)
-                {
-                    Console.WriteLine("Inserisci la lunghezza del primo cateto:");
-                    cateto1Stringa = Console.ReadLine();
-                    if (double.TryParse(cateto1Stringa, out cateto1))
-                        break;
-                    else
-                    {
-                        Console.WriteLine($"{cateto1Stringa} non è un valore corretto");
-                        continue;
-                    }
-                }
-                //Ciclo infinito per chiedere un valore corretto di cateto2
-                while (true)
-                {
-                    Console.WriteLine("Inserisci la lunghezza del secondo cateto:");
-                    cateto2Stringa = Console.ReadLine();
-                    if (double.TryParse(cateto2Stringa, out cateto2))
-                        break;
-                    else
-                    {
-                        Console.WriteLine($"{cateto2Stringa} non è un valore corretto");
-                        continue;
-                    }
-                }
+                cateto1 = ChiediCatetoAllUtente("Inserisci la lunghezza del primo cateto:");
+                cateto2 = ChiediCatetoAllUtente("Inserisci la lunghezza del secondo cateto");
+
                 //Calcolo il valore dell'ipotenusa
                 ipotenusa = Math.Sqrt(Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2));
                 //Calcolo gli angoli
@@ -62,9 +54,10 @@ namespace UnicamMath
                 //Creo due variabili string per arrotondare a due cifre decimali i valori degli angoli
                 string angolo1String = angolo1Gradi.ToString("N2");
                 string angolo2String = angolo2Gradi.ToString("N2");
+                string ipotenusaString = ipotenusa.ToString("N2");
 
                 //Scrivo su console i risultati ottenuti
-                Console.WriteLine($"Cateto1 = {cateto1}; Cateto2 = {cateto2}; Ipotenusa = {ipotenusa}");
+                Console.WriteLine($"Cateto1 = {cateto1}; Cateto2 = {cateto2}; Ipotenusa = {ipotenusaString}");
                 Console.WriteLine($"AngoloRetto = {angoloRetto}; Angolo1 = {angolo1String}; Angolo2 = {angolo2String}");
 
                 //Aspetto che qualcuno prema invio per terminare il programma
